0c7c8c43a8dc6d7a3b3a5e1a6c1d1c5ad4c0661c
william <ishimwewil005@gmail.com>
2020-07-15 11:34:36 +0200

    Configure react environment

---
+++ my-app/src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WelcomeMessage from "../components/Welcome";
+
+const Home = () => {
+  return (
+    <div>
+      <WelcomeMessage/>
+    </div>
+  );
+};
+export default Home;
